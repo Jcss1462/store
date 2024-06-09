cd8fed62b6d5e3767501dd0ac9115d3f9a09a928
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-06-09 16:54:21 -0500

    Se configura Tailwind, y se crean inputs y outputs de componentes

---
+++ src/app/domains/products/pages/list/list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ProductComponent } from './../../components/product/product.component';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [ProductComponent],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  fromChild(event: string){
+
+    console.log("estoy en el padre");
+    console.log(event);
+
+  }
+
+}
